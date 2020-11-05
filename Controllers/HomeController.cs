9298a33e7c056fc86c6352e3b5b8545d387aa8c7
Arpit Mandal <workforarpit@gmail.com>
2020-11-05 12:27:11 +0530

    MVC Client with login page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClientApplication.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace ClientApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Getting the token a Validating the User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            string token = GetToken("https://localhost:44385/api/Token", user);
+
+            if (token != null)
+            {
+                return RedirectToAction("Dashboard", "Home", new { name = token });
+            }
+            else
+            {
+                ViewBag.invalid = "UserId or Password invalid";
+                return View();
+            }
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        static string GetToken(string url, Users user)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(url, data).Result;
+                string name = response.Content.ReadAsStringAsync().Result;
+                dynamic details = JObject.Parse(name);
+                return details.token;
+            }
+        }
+    }
+}
